b5985f72b0a84c39738767b88230d9d5293f09d7
Chen Frederick <frederickfd88@gmail.com>
2021-09-27 22:35:41 +0700

    Updated Form Component Unit Test

--- tests/unit/FormComponent.spec.js
+++ tests/unit/FormComponent.spec.js
@@ -15,16 +15,50 @@ describe("FormComponent", () => {
     expect(wrapper.findComponent(FormComponent).exists()).toBeTruthy();
   });
 
-  it("horizontal line shows the right color styling", () => {
+  it("horizontal line shows the right color styling", async () => {
     const wrapper = factory();
     const horizontalLine = wrapper.find('[data-testid="form-line"]');
     expect(horizontalLine.element.style.borderColor).toBe("red");
+
+    // ----------------------
+    // exist form result case
+    // ----------------------
+    const usernameInput = wrapper.find('[data-testid="username-input"]');
+    const passwordInput = wrapper.find('[data-testid="password-input"]');
+    const submitButton = wrapper.find('[data-testid="submit-btn"]');
+
+    usernameInput.element.value = "fred";
+    usernameInput.trigger("input");
+
+    passwordInput.element.value = "new-password";
+    passwordInput.trigger("input");
+
+    await submitButton.trigger("click");
+
+    expect(horizontalLine.element.style.borderColor).toBe("lightgreen");
   });
 
-  it("form result section is hidden while there are no any result", () => {
+  it("form result section is hidden while there are no any result", async () => {
     const wrapper = factory();
     const formResult = wrapper.find('[data-testid="form-result"]');
     expect(formResult.classes()).toContain("form-result--hidden");
+
+    // ----------------------
+    // exist form result case
+    // ----------------------
+    const usernameInput = wrapper.find('[data-testid="username-input"]');
+    const passwordInput = wrapper.find('[data-testid="password-input"]');
+    const submitButton = wrapper.find('[data-testid="submit-btn"]');
+
+    usernameInput.element.value = "fred";
+    usernameInput.trigger("input");
+
+    passwordInput.element.value = "new-password";
+    passwordInput.trigger("input");
+
+    await submitButton.trigger("click");
+
+    expect(formResult.classes()).not.toContain("form-result--hidden");
   });
 
   it("it successfully submit form", async () => {
